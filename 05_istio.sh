d3010e78cb82d1ab0e5605fac0c7ba6068f9d7ee
Miguel A. Munoz <magonzalez@fortinet.com>
2019-07-29 18:52:35 +0200

    Istio installation

---
+++ 05_istio.sh
@@ -0,0 +1,19 @@
+# Install crd and prerequisites
+curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.2.2 sh - 
+cd istio-1.2.2 
+
+export PATH=$PWD/bin:$PATH 
+
+for i in install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done 
+
+# Install Istio
+kubectl apply -f install/kubernetes/istio-demo.yaml 
+
+
+# Check services and pods from istio
+kubectl get svc -n istio-system 
+kubectl get pods -n istio-system 
+
+ 
+# Set automatic istio injection for every pod
+kubectl label namespace default istio-injection=enabled 
