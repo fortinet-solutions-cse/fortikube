6896afc4220bbace77103e615d0a667a9797ca22
Miguel A. Munoz <magonzalez@fortinet.com>
2019-09-17 19:27:18 +0200

    Adapt Istio definitions to deploy succesfully a FWB with backend

--- 09_deploy_fwb_istio_based.sh
+++ 09_deploy_fwb_istio_based.sh
@@ -11,7 +11,7 @@ metadata:
 spec:  
   ports:  
   - name: http  
-    port: 8000  
+    port: 8008  
     targetPort: 8  
   selector:  
     app: fwb  
@@ -44,26 +44,10 @@ spec:
         version: v1  
     spec:  
       containers:  
-      - image: fwb-image  
-        imagePullPolicy: IfNotPresent  
+      - image: fwb-image
+        imagePullPolicy: Never  
         name: fwb  
-        ports:  
-        - containerPort: 8 
-        - containerPort: 80 
-        - containerPort: 22 
-        resources: 
-          limits: 
-            memory: "4096Mi" 
-          requests: 
-            memory: "4096Mi" 
-        securityContext: 
-          privileged: true 
-          allowPrivilegeEscalation: true 
-          capabilities: 
-            add: 
-              - NET_ADMIN 
-              - NET_RAW 
-              - SYS_PTRACE               
+         
 EOF
    
 # Deploy Ingress gateway from Istio for FWB
@@ -82,54 +66,96 @@ spec:
       name: http  
       protocol: HTTP  
     hosts:  
-    - "fwb.example.com"  
+    - "*"  
+
 EOF
   
 # Deploy a VirtualService to route traffic properly
 
 kubectl apply -f - <<EOF 
-apiVersion: networking.istio.io/v1alpha3  
-kind: VirtualService  
-metadata:  
-  name: fwb  
-spec:  
-  hosts:  
-  - "fwb.example.com"  
-  gateways:  
-  - fwb-gateway  
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: fwb
+spec:
+  hosts:
+  - "*"
+  gateways:
+  - fwb-gateway
   http:
   - match:
     - uri:
-        prefix: /login  
-    route: 
-    - destination: 
+        prefix: /login
+    - uri:
+        prefix: /fgt_lang
+    - uri:
+        prefix: /css
+    - uri:
+        prefix: /util
+    - uri:
+        prefix: /index
+    - uri:
+        prefix: /js
+    - uri:
+        prefix: /fonts
+    - uri:
+        prefix: /console
+    - uri:
+        prefix: /ng
+    - uri:
+        prefix: /system
+    - uri:
+        prefix: /wij
+    - uri:
+        prefix: /container
+    - uri:
+        prefix: /menu
+    - uri:
+        prefix: /module
+    - uri:
+        prefix: /dashboard
+    route:
+    - destination:
+        host: fwb-mgmt
         port:
-          number: 8000  
-        host: fwb-mgmt 
-  - route: 
-    - destination: 
-        port: 
-          number: 8080 
-        host: fwb-traffic 
+          number: 8008
+  - match:
+    - uri:
+        exact: /productpage
+    - uri:
+        prefix: /static
+    - uri:
+        exact: /logout
+    - uri:
+        prefix: /api/v1/products
+    route:
+    - destination:
+        host: fwb-traffic
+        port:
+          number: 8080
 EOF
- 
+                         
 
 # Use these host/port to access Istio Ingress
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}') 
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}') 
 export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}') 
+echo ${INGRESS_HOST}:${INGRESS_PORT}
 
 # Access FWB (mgmt) using the service (as ClusterIP)
 curl -I 10.111.198.90:8000/login 
 
+# Access Sample APP using the service (as ClusterIP)
+curl 10.100.18.55:8080/productpage
+
 # Access FWB (mgmt) Using Istio Ingress
 curl -I -HHost:fwb.example.com ${INGRESS_HOST}:${INGRESS_PORT}/login 
 
-# Access FWB (traffic) Using Istio Ingress
+# Access APP Using Istio Ingress, through FWB
 curl -I -HHost:fwb.example.com ${INGRESS_HOST}:${INGRESS_PORT}/productpage 
 
 # Check ingress logs
-kubectl logs -f istio-ingressgateway-75ddf64567-xb8vf -n istio-system 
+kubectl logs -f istio-ingressgateway-75ddf64567-97r5q -n istio-system 
 
 # Port forward for mgmt HTTP and SSH
 kubectl port-forward --address 0.0.0.0 deploy/fwb 2008:8 2022:22 
