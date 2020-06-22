4dad9babb4534ef9733466416d8c0c92cea4333d
M Angel <m.angel890@gmail.com>
2020-06-22 18:38:47 +0200

    Proper DNS names

--- fortiweb/test_K8S_PetStore.sh
+++ fortiweb/test_K8S_PetStore.sh
@@ -17,48 +17,65 @@ PETSTORE_PORT=$(kubectl get svc petstore -o jsonpath='{.spec.ports[0].port}')
 # Get Pets by status
 # #####################################
 
-# Directly to Service
-curl -X GET "http://${PETSTORE_IP}:${PETSTORE_PORT}/v3/pet/findByStatus?status=available" -H  "accept: application/json"|jq
+# Direct to Service
+curl -X GET "http://${PETSTORE_IP}:${PETSTORE_PORT}/v3/pet/findByStatus?status=available" -H  "accept:application/json"|jq
 
-# Using Istio Gateway
-curl -X GET "http://${INGRESS_HOST}:${INGRESS_PORT}/v3/pet/findByStatus?status=available" -H  "accept: application/json"
-
-# Via FortiWeb
-curl -X GET "http://192.168.100.40:8080/v3/pet/findByStatus?status=available" -H  "accept: application/json"
+# Direct via Istio Gateway
+curl -X GET "http://${INGRESS_HOST}:${INGRESS_PORT}/v3/pet/findByStatus?status=available" -H  "accept:application/json"
 
 # Direct via Istio - Using DNS
-watch curl  -X GET "http://petstore.com:31380/v3/pet/findByStatus?status=available" -H  "accept: application/json" -H "Host:petstore.com"
+watch curl -X GET "http://petstore.com:31380/v3/pet/findByStatus?status=available" -H "accept:application/json" -H "Host:petstore.com"
 
 # Through FortiWeb via Istio - Using DNS
-watch curl  -X GET "http://petstore-fwb.com:31380/v3/pet/findByStatus?status=available" -H  "accept: application/json" -H "Host:petstore-fwb.com"
+watch curl -X GET "http://petstore-fwb.com:31380/v3/pet/findByStatus?status=available" -H  "accept:application/json" -H "Host:petstore-fwb.com"
 
+# Through FortiWeb (using external FWB GW, as VM) (Legacy)
+curl -X GET "http://192.168.100.40:8080/v3/pet/findByStatus?status=available" -H  "accept:application/json"
 
 
 # #####################################
 # Test Wrong Parameters
 # #####################################
 
-# Directly to Service
-curl -X GET "http://${PETSTORE_IP}:${PETSTORE_PORT}/v3/pet/findByStatus?status=1" -H  "accept: application/json"|jq
+# Direct to Service
+curl -X GET "http://${PETSTORE_IP}:${PETSTORE_PORT}/v3/pet/findByStatus?status=1" -H  "accept:application/json"|jq
 
-# Using Istio Gateway
-curl -X GET "http://${INGRESS_HOST}:${INGRESS_PORT}/v3/pet/findByStatus?status=1" -H  "accept: application/json"
+# Direct via Istio Gateway
+curl -X GET "http://${INGRESS_HOST}:${INGRESS_PORT}/v3/pet/findByStatus?status=1" -H  "accept:application/json"
 
-# Via FortiWeb
-curl -X GET "http://192.168.100.40:8080/v3/pet/findByStatus?status=1" -H  "accept: application/json"
+# Through FortiWeb (using external FWB GW, as VM) (Legacy)
+curl -X GET "http://192.168.100.40:8080/v3/pet/findByStatus?status=1" -H  "accept:application/json"
 
 
 # #####################################
 # Test Wrong Calls
 # #####################################
 
-# Directly to Service
-curl -X GET "http://${PETSTORE_IP}:${PETSTORE_PORT}/v3/pet/findByStatusWRONGCALL?status=available" -H  "accept: application/json"|jq
+# Direct to Service
+curl -X GET "http://${PETSTORE_IP}:${PETSTORE_PORT}/v3/pet/findByStatusWRONGCALL?status=available" -H  "accept:application/json"|jq
 
-# Using Istio Gateway
-curl -X GET "http://${INGRESS_HOST}:${INGRESS_PORT}/v3/pet/findByStatusWRONGCALL?status=available" -H  "accept: application/json"
+# Direct via Istio Gateway
+curl -X GET "http://${INGRESS_HOST}:${INGRESS_PORT}/v3/pet/findByStatusWRONGCALL?status=available" -H  "accept:application/json"
 
-# Via FortiWeb
-curl -X GET "http://192.168.100.40:8080/v3/pet/findByStatusWRONGCALL?status=available" -H  "accept: application/json"
+# Through FortiWeb (using external FWB GW, as VM) (Legacy)
+curl -X GET "http://192.168.100.40:8080/v3/pet/findByStatusWRONGCALL?status=available" -H  "accept:application/json"
 
+
+# #####################################
+# Test Users accessing different urls
+# #####################################
+
+# Direct via Istio - Using DNS
+curl -X GET "http://petstore.com:31380/v3/pet/findByStatus?status=available" -H "accept:application/json" -H "Host:petstore.com"
+curl -X GET "http://petstore.com:31380/v3/store/inventory" -H "accept:application/json" -H "Host:petstore.com"
+
+# Through FortiWeb via Istio - Using DNS
+curl -X GET "http://petstore-fwb.com:31380/v3/pet/findByStatus?status=available" -H  "accept:application/json" -H "Host:petstore-fwb.com"
+curl -X GET "http://petstore-fwb.com:31380/v3/store/inventory" -H "accept:application/json" -H "Host:petstore-fwb.com"
+
+# User A accessing correctly findByStatus api call
+curl -X GET "http://petstore-fwb.com:31380/v3/pet/findByStatus?status=available" -H "accept:application/json" -H "Host:petstore-fwb.com" -H "token:XXXXX"
+
+# User B accessing correctly store inventory
+curl -X GET "http://petstore-fwb.com:31380/v3/store/inventory?token=XXXXXX" -H "accept:application/json" -H "Host:petstore-fwb.com"
 
